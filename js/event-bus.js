983f2a3c53f021f5b33b82c1b8ef622e73454950
brossardd <44892766+brossardd@users.noreply.github.com>
2018-11-29 15:36:33 +0100

    Create event-bus.js

---
+++ js/event-bus.js
@@ -0,0 +1,31 @@
+const { Observable, Subject, fromEvent, pipe } = rxjs;
+const { postMessage, filter, map } = rxjs.operators;
+
+class PostMessageEventBus {
+  
+  constructor(origin, target) {
+    this.getMessages.bind(this);
+    this.sendMessage.bind(this);
+      
+    this.messages = fromEvent(origin, 'message');
+    this.target = target;
+  }
+ 
+  getMessages(messageType) {
+    return this.messages
+      .pipe(
+        map(message => JSON.parse(message.data)),
+        filter(messageData => messageData.type === messageType)
+    );
+  }
+ 
+  sendMessage(messageType, data) {
+    this.target.postMessage(JSON.stringify({
+      type: messageType,
+      data: data
+    }), '*');
+  }
+}
+
+
+
