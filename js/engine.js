93807571bcc5b2ef3d864844c24bbe0653761ac0
brossardd <44892766+brossardd@users.noreply.github.com>
2018-11-29 15:42:28 +0100

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -5,11 +5,14 @@
       const iframe = document.createElement("iframe");
       iframe.setAttribute("src", "./pres.html");
       document.body.appendChild(iframe);
+      return iframe;
     }
     
     
     window.addEventListener('DOMContentLoaded', function() {
-      createIframe();
+      const iframe = createIframe();
+      const postMessageEventBus = new PostMessageEventBus(window, iframe.contentWindow);
+      postMessageEventBus.sendMessage('INIT', {message: 'Initialisation '});
     });
   }
 )();
