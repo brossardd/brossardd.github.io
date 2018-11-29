28de7675975a54bd4b3fc48767d98ca3a0339625
brossardd <44892766+brossardd@users.noreply.github.com>
2018-11-29 15:58:11 +0100

    Update pres-engine.js

--- js/pres-engine.js
+++ js/pres-engine.js
@@ -1,1 +1,15 @@
-
+(
+  function() {
+    
+    function init(){
+      const postMessageEventBus = new PostMessageEventBus(window, iframe.contentWindow);
+      postMessageEventBus.getMessages('INIT').subscribe(onInitMessage);
+    }
+    
+    function onInitMessage(message){
+      console.log(message.data);
+    }
+    
+    window.addEventListener('DOMContentLoaded', init);
+  }
+)();
