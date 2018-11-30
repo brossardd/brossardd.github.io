7555337aa17c8fb65941605090b6b7a07619240f
brossardd <44892766+brossardd@users.noreply.github.com>
2018-11-30 14:54:18 +0100

    Update pres-engine.js

--- js/pres-engine.js
+++ js/pres-engine.js
@@ -16,8 +16,8 @@
     function onMessage(message){
       console.log('PRES : Message Received');
       console.log(`Message Type : ${message.type}`);
-      const messageString = JSON.stringify(message);
-        console.log(`Message : ${messageString}`);
+      const messageData = JSON.stringify(message.data);
+      console.log(`Message : ${messageString}`);
     }
     
     window.addEventListener('DOMContentLoaded', init);
