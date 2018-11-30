143466597610b34b1687f493389705a12c303228
brossardd <44892766+brossardd@users.noreply.github.com>
2018-11-30 14:25:54 +0100

    Update pres-engine.js

--- js/pres-engine.js
+++ js/pres-engine.js
@@ -4,12 +4,20 @@
     function init(){
       console.log('Init presentation');
       const postMessageEventBus = new PostMessageEventBus(window, window.parent);
+      postMessageEventBus.getMessages().subscribe(onMessage);
       postMessageEventBus.getMessages('SNAPSHOT').subscribe(onSnapshotMessage);
       postMessageEventBus.sendMessage('INIT', 'Presentation initialized');
     }
     
     function onSnapshotMessage(message){
       console.log('pres.postMessageEventBus.onSnapshotMessage');
+    }
+    
+    function onMessage(message){
+      console.log('Message Received :');
+      console.log(`Message Type : ${message.type}`);
+      const messageData = JSON.stringify(message.data);
+      console.log(`Message Data : ${messageData}`);
     }
     
     window.addEventListener('DOMContentLoaded', init);
