c71317159593a0f95fa4755b481c85ca0076a4fb
brossardd <44892766+brossardd@users.noreply.github.com>
2018-11-30 15:24:49 +0100

    Update layout-engine.js

--- js/layout-engine.js
+++ js/layout-engine.js
@@ -1,44 +1,45 @@
-(
-  function() {
-    
-    function init(){
-      const iframe = createIframe();
-      const postMessageEventBus = new PostMessageEventBus(window, iframe.contentWindow);
-      postMessageEventBus.getMessages('INIT').subscribe(onInitMessage);
-      postMessageEventBus.getMessages().subscribe(onMessage);
+function init(){
+  const iframe = createIframe();
+  const postMessageEventBus = new PostMessageEventBus(window, iframe.contentWindow);
+  postMessageEventBus.getMessages('INIT').subscribe(onInitMessage);
+  postMessageEventBus.getMessages().subscribe(onMessage);
 
-      const websocketEventBus = new MockWebSocketEventBus();
-      websocketEventBus.getMessages('SNAPSHOT').subscribe(onSnapshotMessage);
-      
-      function onInitMessage(message){
-        console.log('layout.postMessageEventBus.onInitMessage');
-        websocketEventBus.sendMessage('REQUEST-SNAPSHOT', 'Request snapshot message');
-      }
-      
-      function onSnapshotMessage(message){
-        console.log('layout.websocketEventBus.onSnapshotMessage');
-        postMessageEventBus.sendMessage('SNAPSHOT', {});
-      }
-      
-      function onMessage(message){
-        console.log('LAYOUT : Message Received');
-        console.log(`Message Type : ${message.type}`);
-        const messageData = JSON.stringify(message.data);
-        console.log(`Data : ${messageData}`);
-      }
-    }
-    
-    function createIframe(){
-      console.log( "Create iframe" );
-      const iframe = document.createElement("iframe");
-      iframe.setAttribute("src", "./pres.html");
-      document.body.appendChild(iframe);
-      return iframe;
-    }
-    
-    window.addEventListener('DOMContentLoaded', init);
+  const websocketEventBus = new MockWebSocketEventBus();
+  websocketEventBus.getMessages('SNAPSHOT').subscribe(onSnapshotMessage);
+  
+  function onInitMessage(message){
+    console.log('layout.postMessageEventBus.onInitMessage');
+    websocketEventBus.sendMessage('REQUEST-SNAPSHOT', 'Request snapshot message');
   }
-)();
+  
+  function onSnapshotMessage(message){
+    console.log('layout.websocketEventBus.onSnapshotMessage');
+    postMessageEventBus.sendMessage('SNAPSHOT', {});
+  }
+  
+  function onMessage(message){
+    console.log('LAYOUT : Message Received');
+    console.log(`Message Type : ${message.type}`);
+    const messageData = JSON.stringify(message.data);
+    console.log(`Data : ${messageData}`);
+  }
+  
+  function createIframe(){
+    console.log( "Create iframe" );
+    const iframe = document.createElement("iframe");
+    iframe .id = 'stageFrame';
+    iframe.setAttribute("src", "./pres.html");
+    document.body.appendChild(iframe);
+    return iframe;
+  }
+}
+
+
+
+
+
+
+
 
 
 
