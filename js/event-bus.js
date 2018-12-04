9a047c3117ae4b25160d1cf139ed262d11fce608
brossardd <44892766+brossardd@users.noreply.github.com>
2018-12-04 16:18:06 +0100

    Update event-bus.js

--- js/event-bus.js
+++ js/event-bus.js
@@ -54,31 +54,31 @@ class WebSocketEventBus {
   */ 
   constructor(url) {
     this.subject$ = Observable.webSocket(url);
-    
-    /** @description Returns an observable of the incoming messages.
-    * The messages can be filtered if the messageTypeRegex is not null.
-    * @param {string} messageType The message type regex used to filter the incoming messages
-    * @return {Observable} An observable of the incoming messages.
-    */
-    this.getMessages = messageType => {
-      if(!messageType) {
-        return this.messages;
-      }
-      return this.subject$.multiplex(
-        () => JSON.stringify({subscribe: messageType}),
-        () => JSON.stringify({unsubscribe: messageType}),
-        message => message.type.match(messageType));
-    };
-
-   /** @description Send a message.
-    * @param {Object} message The message
-    * @param {string} message.type The message type.
-    * @param {string} message.data The message data.
-    */
-    this.sendMessage = ({type, data}) => {
-      this.subject$.next({type, data});
-    };
   }
+  
+  /** @description Returns an observable of the incoming messages.
+  * The messages can be filtered if the messageTypeRegex is not null.
+  * @param {string} messageType The message type regex used to filter the incoming messages
+  * @return {Observable} An observable of the incoming messages.
+  */
+  getMessages = messageType => {
+    if(!messageType) {
+      return this.messages;
+    }
+    return this.subject$.multiplex(
+      () => JSON.stringify({subscribe: messageType}),
+      () => JSON.stringify({unsubscribe: messageType}),
+      message => message.type.match(messageType));
+  };
+
+  ** @description Send a message.
+  * @param {Object} message The message
+  * @param {string} message.type The message type.
+  * @param {string} message.data The message data.
+  */
+  sendMessage = ({type, data}) => {
+    this.subject$.next({type, data});
+  };
 }
 
 /** @description Reactive event bus used to send and receive messages via
